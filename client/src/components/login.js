ac5a309ec724d65bc633aadbb1f37f48d291265f
Gavin Clay <cgavin40@yahoo.com>
2018-01-08 10:39:45 -0500

    canvas and FE components

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,8 +1,8 @@
-
 //This is the Login page that the user will see when first going to the page. They have to click on the login button which will redirect them to Bot setup.
 
 import React from 'react';
 import { Link } from 'react-router';
+import '../BotSetting.js';
 
 export default class Login extends React.Component {
     constructor(props){
@@ -32,7 +32,7 @@ export default class Login extends React.Component {
                         <p className="intro"> An interactive twitch stream application!
                         </p>
                         <p className="intro2"> To get started, login with your Twitch or Google credentials below. </p>
-                        <button className="btn btn-primary loginBtn" onClick={()=>this.handleClick()}>Log in with Twitch or Google.
+                        <button className="btn btn-primary loginBtn" onClick={()=>this.handleClick()}><a href="/auth/twitch"><img src="http://ttv-api.s3.amazonaws.com/assets/connect_dark.png">Log in with Twitch or Google.</a>
                         </button>
                     </div>
                 </div>
