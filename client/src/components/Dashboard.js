06c2dc9213d275d69de1b832d8bff6c01365a485
gitKendra <kendra.krzywicki@knights.ucf.edu>
2018-01-13 12:49:26 -0500

    hardcode default user channel

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -9,23 +9,23 @@ class Dashboard extends Component {
 
     state = {
         username: "",
-        channel: "",
+        channel: "digifriend",
         logo: "",
         id: ""
     }
 
     componentDidMount() {
-        // Get user data from db to display customized dashboard
-        helpers.getUser("5a58fde1b22f3594888cd306")
-        .then( (user) => {
-            var channel = user.data.botSettings.channels[0].substring(1);
-            this.setState({
-                username: user.data.username,
-                channel: channel,
-                logo: user.data.logo,
-                id: user.data._id
-            });
-        });
+        // // Get user data from db to display customized dashboard
+        // helpers.getUser("5a5a4271bd5601fb56ccffb7")
+        // .then( (user) => {
+        //     var channel = user.data.botSettings.channels[0].substring(1);
+        //     this.setState({
+        //         username: user.data.username,
+        //         channel: channel,
+        //         logo: user.data.logo,
+        //         id: user.data._id
+        //     });
+        // });
     }
 
 
