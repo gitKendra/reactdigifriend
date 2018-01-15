58df91859f9215d598f5bcefaaa41213ae08b92b
Kendra Krzywicki <kendra.krzywicki@knights.ucf.edu>
2018-01-15 12:10:30 -0500

    chatbot connects with tmi.js

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,27 +4,50 @@ import Login from './Login';
 import helpers from '../utils/helpers';
 import Chatbox from './Dashboard/Chatbox';
 import ToggleBot from './Dashboard/ToggleBot';
+import tmi from "tmi.js";
 
 class Dashboard extends Component {
 
     state = {
         username: "",
-        channel: "",
         logo: "",
-        id: ""
+        id: "",
+        channel: "",
+        client: null
     }
 
     componentDidMount() {
         // Get user data from db to display customized dashboard
         helpers.getUser("5a58fde1b22f3594888cd306")
         .then( (user) => {
-            var channel = user.data.botSettings.channels[0].substring(1);
+            // var channel = user.data.botSettings.channels[0].substring(1);
+            const botSettings = user.data.botSettings;
+
             this.setState({
                 username: user.data.username,
-                channel: channel,
                 logo: user.data.logo,
-                id: user.data._id
+                id: user.data._id,
+                channel: botSettings.channels[0].substring(1)
             });
+
+            // Create and setup bot
+            var client = tmi.client({
+                options: {debug: true},
+                connection: {reconnect: true},
+                identity: {username: botSettings.name, password: botSettings.oAuth},
+                channels: botSettings.channels
+            });
+
+            // Connect to chat server
+            client.connect().then(data => {
+                console.log(`Connected to ${data[0]} on port ${data[1]}`);
+                // Join chat server
+                client.join(this.state.channel).then(channel => {
+                  console.log(`joined ${channel} channel`);
+                });
+              });
+          
+            this.setState({ client: client });
         });
     }
 
@@ -37,9 +60,11 @@ class Dashboard extends Component {
 
                 <ToggleBot />
 
-                {this.state.channel !== "" && 
-                    <Chatbox channel={this.state.channel} />
+                {this.state.client !== null && 
+                    <Chatbox channel={this.state.channel} botClient={this.state.client} />
                 }
+
+
 
             </div>
         );
